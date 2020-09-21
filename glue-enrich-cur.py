30652cdcf83228d6632b83adbac675cd2fe1e930
Chris <chorder@amazon.com>
2020-09-22 01:31:44 +1000

    Fixed athena, merged_df missing from glude definition

--- glue-enrich-cur.py
+++ glue-enrich-cur.py
@@ -68,7 +68,6 @@ arg_parser.add_argument('--s3_target_prefix', required=True, help="The destinati
 arg_parser.add_argument('--incremental_mode_months', type=int, required=False, default=0, help="When set, last x months of CUR data is processed. When not set or set to 0, all data is processed. For incremental updates, recommend setting this to 2.")
 arg_parser.add_argument('--create_table', action='store', required=False, default=False, help="When set, a table will be created in Glue automatically and made available through Athena.")
 arg_parser.add_argument('--overwrite_existing_table', type=bool, action='store', required=False, default=False, help="When set, the existing table will be overwritten. This should be set when performing updates.")
-arg_parser.add_argument('--partition_by_account', type=bool, action='store', required=False, default=False, help="When set, an additional partition for line_item_usage_account_id will be created. Example: /year=2020/month=1/line_item_usage_account_id=123456789012")
 arg_parser.add_argument('--database_name', type=str, required=False, default=None, help="The name of the Glue database to create the table in. Must be set when --create_table is set")
 arg_parser.add_argument('--table_name', type=str, required=False, default=None, help="The name of the Glue table to create or overwrite. Must be set when --create_table is set")
 arg_parser.add_argument('--extra-py-files', type=str, required=False, default=None)
@@ -84,7 +83,8 @@ S3_SOURCE_PREFIX = args["s3_source_prefix"]
 S3_TARGET_BUCKET = args["s3_target_bucket"]
 S3_TARGET_PREFIX = args["s3_target_prefix"]
 INCREMENTAL_MODE_MONTHS = args["incremental_mode_months"]
-PARTITION_BY_ACCOUNT = args['partition_by_account']
+PARTITION_BY_ACCOUNT = False
+
 CREATE_TABLE = args["create_table"]
 DATABASE_NAME = args["database_name"]
 TABLE_NAME = args["table_name"]
@@ -120,7 +120,7 @@ account_tags=account_tags.convert_dtypes()
 for tagKey in uniqueTagKeys:
 #   3. Use .apply with a lambda function on the 'Tags' column to retrieve nested 'Value' in each item in the column, if it exists
 #   4. Set the value of the new column to the series that was returned by .apply
-    account_tags['tag_' + tagKey] = accounts['Tags'].apply(lambda x: get_kv_pair_value(pd.DataFrame(x, columns=['Key', 'Value']).set_index('Key'),tagKey))
+    account_tags['account_tag_' + tagKey] = accounts['Tags'].apply(lambda x: get_kv_pair_value(pd.DataFrame(x, columns=['Key', 'Value']).set_index('Key'),tagKey))
 # Note: A lambda (get_kv_pair_value) is used so that it can handle exceptions when the tag does not exist for a particular account or the account had no tags at all
 
 account_tags.set_index("account_tag__account_id")
@@ -128,9 +128,10 @@ print("Account tags DataFrame")
 account_tags.info()
 
 # Get a list of objects
-print("Listing objects in path: {}".format("s3://"+S3_SOURCE_BUCKET+"/"+S3_SOURCE_PREFIX))
+s3_orginal_path = "s3://"+S3_SOURCE_BUCKET+"/"+S3_SOURCE_PREFIX
+print("Listing objects in path: {}".format(s3_orginal_path))
+s3_objects = wr.s3.list_objects(s3_orginal_path)
 
-s3_objects = wr.s3.list_objects("s3://"+S3_SOURCE_BUCKET+"/"+S3_SOURCE_PREFIX)
 # Sort objects by month, adding leading zeros to the months to sort correctly
 s3_objects.sort(key=lambda x: re.sub(r'(month=)(\d+)', lambda m : m.group(1)+m.group(2).zfill(2),x))
 print("Objects found: {}".format(s3_objects))
@@ -145,7 +146,11 @@ for s3_obj in s3_objects:
   if not s3_obj.endswith(".parquet"):
     print('SKIPPING NON PARQUET FILE: {}'.format(s3_obj))
     continue
-  print('READING: {}'.format(s3_obj))
+  
+  current_file = s3_obj.split("/")[-1]
+  current_path = s3_obj.replace(s3_orginal_path, "").replace(current_file, "")
+  print('READING: {} from path {} and file {}'.format(s3_obj, current_path, current_file))
+
   df = wr.s3.read_parquet(path=s3_obj)
   print("=== RAW BEGIN DATAFRAME INFO ===")
   df.info()
@@ -162,20 +167,29 @@ for s3_obj in s3_objects:
 
   with pd.option_context('display.max_rows', None, 'display.max_columns', None): print(merged_df.columns)
   print("=== END DATAFRAME INFO ===")
-  print ("WRITING TO PATH: {}".format("s3://"+S3_TARGET_BUCKET+"/"+S3_TARGET_PREFIX+"/"+ wr._utils.parse_path(s3_obj)[1].rsplit("/",1)[0]))
-  #print ("WRITING TO PATH: {}".format("s3://"+S3_TARGET_BUCKET+"/"+S3_TARGET_PREFIX+"/"+ wr._utils.parse_path(s3_obj)[1]))
+  print ("WRITING TO PATH: {}".format("s3://" + S3_TARGET_BUCKET + "/" + S3_TARGET_PREFIX))
+
   s3_written_objs = wr.s3.to_parquet(
     df = merged_df,
-    path = "s3://"+S3_TARGET_BUCKET+"/"+S3_TARGET_PREFIX+"/"+ wr._utils.parse_path(s3_obj)[1].rsplit("/",1)[0],
+    path = "s3://" + S3_TARGET_BUCKET + "/" + S3_TARGET_PREFIX + current_path,
     partition_cols=  ["line_item_usage_account_id"] if PARTITION_BY_ACCOUNT else [],
     mode = "overwrite",
     compression="snappy",
     index=False,
     dataset = True
-
   )
+
   print("Wrote: {}".format(s3_written_objs))
   print("=============")
+
+
+databases = wr.catalog.databases()
+
+if DATABASE_NAME not in databases.values:
+    wr.catalog.create_database(DATABASE_NAME)
+    print(wr.catalog.databases())
+else:
+    print("Database {} already exists".format(DATABASE_NAME))
 
 if CREATE_TABLE:
   if OVERWRITE_EXISTING_TABLE:
@@ -186,13 +200,13 @@ if CREATE_TABLE:
     )
   
 
-  print ("Creating Table {}.{} with path {}".format(DATABASE_NAME, TABLE_NAME, "s3://"+S3_TARGET_BUCKET+"/"+S3_TARGET_PREFIX+"/"+ wr._utils.parse_path(s3_obj)[1].rsplit("/",3)[0]+"/"))
+  print ("Creating Table {}.{} with path {}".format(DATABASE_NAME, TABLE_NAME, "s3://" + S3_TARGET_BUCKET + "/" + S3_TARGET_PREFIX)) 
   wr.catalog.create_parquet_table(
-    path = "s3://"+S3_TARGET_BUCKET+"/"+S3_TARGET_PREFIX+"/"+wr._utils.parse_path(s3_obj)[1].rsplit("/",3)[0]+"/",
+    path = "s3://" + S3_TARGET_BUCKET + "/" + S3_TARGET_PREFIX,
     database = DATABASE_NAME,
     table = TABLE_NAME,
-    columns_types= wr._data_types.athena_types_from_pandas(df=df.drop(columns='line_item_usage_account_id') if PARTITION_BY_ACCOUNT else df, index=False),
-    partitions_types={'year': 'string','month':'string','line_item_usage_account_id':'string'} if PARTITION_BY_ACCOUNT else {'year': 'string','month':'string'},
+    columns_types = wr._data_types.athena_types_from_pandas(merged_df, index=False),
+    partitions_types = {'year': 'string','month':'string'} 
   )
 
 
