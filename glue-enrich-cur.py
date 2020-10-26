cec22c0d5f866b32531573ade08a4a25dab83fbd
Chris <chorder@amazon.com>
2020-10-27 00:35:47 +1100

    sam: move to chunk processing

--- glue-enrich-cur.py
+++ glue-enrich-cur.py
@@ -18,7 +18,7 @@
 
 import os
 import sys
-import gc
+
 import awswrangler as wr
 import pandas as pd
 import boto3
@@ -27,17 +27,19 @@ import re
 from functools import reduce
 
 orgs_client = boto3.client('organizations')
+s3_resource = boto3.resource('s3')
 
-# Fetches all accounts from the organizations API
+  # Fetches all accounts from the organizations API
 def get_accounts ():
-    accounts={ 'Accounts':[] }
+    accounts = { 'Accounts':[] }
     paginator = orgs_client.get_paginator('list_accounts')
     page_iterator = paginator.paginate()
     for page in page_iterator:
         accounts['Accounts'] = accounts['Accounts'] + page['Accounts']    
+    
     return accounts    
 
-# Fetches all tags for an AWS Account ID
+  # Fetches all tags for an AWS Account ID
 def get_tags_for_account (account):
     tags={ 'Tags':[] }
     paginator = orgs_client.get_paginator('list_tags_for_resource')
@@ -46,17 +48,18 @@ def get_tags_for_account (account):
     )
     for page in page_iterator:
         tags['Tags'] = tags['Tags'] + page['Tags']    
+    
     return tags
 
-# Returns value of cell 0 at a given index name (idx) from a Dataframe, if it exists. Otherwise, returns None. 
-# This is used to handle situations where the Dataframe doesn't contain the index (i.e. a particular tag isn't used in the account)
+  # Returns value of cell 0 at a given index name (idx) from a Dataframe, if it exists. Otherwise, returns None. 
+  # This is used to handle situations where the Dataframe doesn't contain the index (i.e. a particular tag isn't used in the account)
 def get_kv_pair_value(df, idx):
     try:
         return df.loc[idx][0]
     except KeyError:
         return None
 
-# OPTIONAL ARGUMENT PARSER (getResolvedOptions doesn't support optional arguments at the moment)
+  # OPTIONAL ARGUMENT PARSER (getResolvedOptions doesn't support optional arguments at the moment)
 arg_parser = argparse.ArgumentParser(
     description="Enriches CUR data stored in S3 and enriches S3 by adding organizational account tags as columns to line items.", 
     epilog="NOTES: The script expects the source CUR data to be stored in parquet. The Cost and Usage Report configured to generate this data should be set to aoutput for ATHENA. Organizational account tags are merged based on line_item_usage_account_id. Because this queries the Organizations API, it must be run from the Master Account."
@@ -75,48 +78,39 @@ arg_parser.add_argument('--extra-py-files', type=str, required=False, default=No
 arg_parser.add_argument('--scriptLocation', type=str, required=False, default=None)
 arg_parser.add_argument('--job-bookmark-option', type=str, required=False, default=None)
 arg_parser.add_argument('--job-language', type=str, required=False, default=None)
-print(vars(arg_parser.parse_args() ))
 args = vars(arg_parser.parse_args())
-
 
 S3_SOURCE_BUCKET = args["s3_source_bucket"]
 S3_SOURCE_PREFIX = args["s3_source_prefix"]
 S3_TARGET_BUCKET = args["s3_target_bucket"]
 S3_TARGET_PREFIX = args["s3_target_prefix"]
 INCREMENTAL_MODE_MONTHS = args["incremental_mode_months"]
-PARTITION_BY_ACCOUNT = False
-
 CREATE_TABLE = args["create_table"]
 DATABASE_NAME = args["database_name"]
 TABLE_NAME = args["table_name"]
 OVERWRITE_EXISTING_TABLE = args["overwrite_existing_table"]
-
-LAST_SCHEMA_FROM_CUR = ""
+PARTITION_BY_ACCOUNT = False
 
 if TABLE_NAME == None or DATABASE_NAME == None: raise Exception('Must specify Glue Database and Table when "--create_table" is set')
 if wr.catalog.does_table_exist(DATABASE_NAME,TABLE_NAME) and not OVERWRITE_EXISTING_TABLE: raise Exception('The table {}.{} already exists but OVERWRITE_EXISTING_TABLE isn''t set.'.format(DATABASE_NAME, TABLE_NAME))
 
-#Instance details
-mem_bytes = os.sysconf('SC_PAGE_SIZE') * os.sysconf('SC_PHYS_PAGES')  # e.g. 4015976448
-mem_gib = mem_bytes/(1024.**3)  # e.g. 3.74
-print('Memory on host is {:.2f} GB'.format(mem_gib))
-
-# First we retrieve the accounts through the organizations API
+  # First we retrieve the accounts through the organizations API
 print("Fetching accounts")
 accounts = pd.json_normalize(get_accounts(), record_path='Accounts')  
 accounts.info()
-
+  
+  # Next we retrieve the tags for each account via the organizations API and load them into a new 'Tags' column
 print("Fetching account tags")
-# Next we retrieve the tags for each account via the organizations API and load them into a new 'Tags' column
 accounts['Tags'] = accounts.apply(lambda x: get_tags_for_account(x['Id'])['Tags'], axis=1)
 
-# We need to convert the tag values in each row into columns in our DataFrame. 
-# Each result in the 'Tags' column is list of dict with this format: [{'Key': 'CostCenter', 'Value': 'MyTestCostCenter'}, {'Key': 'BudgetCode', 'Value': 'MyTestBudgetCode'}]
-# 'Key' is the name of the tag, and 'Value' is its value
-# Every account can have different tags, or none at all. So how do we take these tag keys, make them columns, and then fill those columns with the value?
-# The steps below are:
-#   1. Get a list of all unique 'Key' values (tag names).
-uniqueTagKeys=pd.concat([pd.DataFrame(pd.json_normalize(x)) for x in accounts['Tags']], ignore_index=True)['Key'].drop_duplicates().values.tolist()
+  # We need to convert the tag values in each row into columns in our DataFrame. 
+  # Each result in the 'Tags' column is list of dict with this format: [{'Key': 'CostCenter', 'Value': 'MyTestCostCenter'}, {'Key': 'BudgetCode', 'Value': 'MyTestBudgetCode'}]
+  # 'Key' is the name of the tag, and 'Value' is its value
+  # Every account can have different tags, or none at all. So how do we take these tag keys, make them columns, and then fill those columns with the value?
+  # The steps below are:
+
+  #   1. Get a list of all unique 'Key' values (tag names).
+uniqueTagKeys = pd.concat([pd.DataFrame(pd.json_normalize(x)) for x in accounts['Tags']], ignore_index=True)['Key'].drop_duplicates().values.tolist()
 
 print("Creating account tags DataFrame")
 account_tags = pd.DataFrame()
@@ -134,80 +128,85 @@ for tagKey in uniqueTagKeys:
     account_tags = account_tags.astype({new_tag_key : 'string'})
 # Note: A lambda (get_kv_pair_value) is used so that it can handle exceptions when the tag does not exist for a particular account or the account had no tags at all
 
-#Set index for lookups with parquet data set
+  # Set index for lookups with parquet data set
 account_tags.set_index("account_tag__account_id")
 print("Account tags DataFrame")
 account_tags.info()
 
-# Get a list of objects
-s3_orginal_path = "s3://"+S3_SOURCE_BUCKET+"/"+S3_SOURCE_PREFIX
+  # Get a list of objects
+s3_orginal_path = "s3://" + S3_SOURCE_BUCKET + "/" + S3_SOURCE_PREFIX
 print("Listing objects in path: {}".format(s3_orginal_path))
 s3_objects_raw = wr.s3.list_objects(s3_orginal_path, suffix=".parquet")
-#Clean out the files as we want to treat each base folder as a parquet to read
+  
+  # Clean out the files as we want to treat each base folder as a parquet to read
 s3_objects = reduce(lambda l, x: l.append(x.replace(x.split("/")[-1], "")) or l if x.replace(x.split("/")[-1], "") not in l else l, s3_objects_raw, [])
-# Sort objects by month, adding leading zeros to the months to sort correctly
-s3_objects.sort(key=lambda x: re.sub(r'(month=)(\d+)', lambda m : m.group(1)+m.group(2).zfill(2),x))
+  
+  # Sort objects by month, adding leading zeros to the months to sort correctly
+s3_objects.sort(key = lambda x: re.sub(r'(month=)(\d+)', lambda m : m.group(1)+m.group(2).zfill(2), x))
 
 print("Objects found: {}".format(s3_objects))
 
-# If INCREMENTAL_MODE_MONTHS is greater than zero, only use the last INCREMENTAL_MODE_MONTHS items in the list. This assumes everything is partitioned by '/year=xxxx/month=xx/'
+  # If INCREMENTAL_MODE_MONTHS is greater than zero, only use the last INCREMENTAL_MODE_MONTHS items in the list. This assumes everything is partitioned by '/year=xxxx/month=xx/'
 if INCREMENTAL_MODE_MONTHS > 0:
   s3_objects = s3_objects[-INCREMENTAL_MODE_MONTHS:]
   print("Incremental mode set. Filtered objects: {}".format(s3_objects))
 
+  # iterate over s3 keys (not objects, but partitions)
+print("=== Iterating S3 Objects ===")
 for s3_obj in s3_objects:
-  print("=============")  
+  
+  print("=== Iteration " + s3_obj + " ===")
   current_file = s3_obj.split("/")[-1]
   current_path = s3_obj.replace(s3_orginal_path, "").replace(current_file, "")
-  print('READING: {} from path {} and file {}'.format(s3_obj, current_path, current_file))
+  
+  print("removing old enriched files")
+  bucket = s3_resource.Bucket(S3_TARGET_BUCKET)
+  for obj in bucket.objects.filter(Prefix = S3_TARGET_PREFIX + current_path):
+    s3_resource.Object(bucket.name, obj.key).delete()
 
-  # df = wr.s3.read_parquet(path=s3_obj)
-  print(s3_obj.replace(current_file, ""))
-  df = wr.s3.read_parquet(path=s3_obj.replace(current_file, ""))
-  print("=== RAW BEGIN DATAFRAME INFO ===")
-  df.info()
+  print('creating dataframe chunks: {} from path {} and file {}'.format(s3_obj, current_path, s3_obj.replace(current_file, "")))
+  df_chunks = wr.s3.read_parquet(path=s3_obj.replace(current_file, ""), chunked = True)
 
-  print("=== RAW END DATAFRAME INFO ===")
-  with pd.option_context('display.max_rows', None, 'display.max_columns', None): print(df.columns)
-  df.set_index('identity_line_item_id')
-  print('Merging account tags')
-  df = df.merge(account_tags, left_on='line_item_usage_account_id', right_on='account_tag__account_id', how="left")
-  df.drop(columns=['account_tag__account_id'], inplace=True)
-  print("=== BEGIN DATAFRAME INFO ===")
-  df.info()
-  print("Original Rows: {} Merged Rows: {}".format(df.shape[0], df.shape[0]))
+  print("=== Iterating df chunks ===")
+  for df in df_chunks:
 
-  with pd.option_context('display.max_rows', None, 'display.max_columns', None): print(df.columns)
-  print("=== END DATAFRAME INFO ===")
-  print ("WRITING TO PATH: {}".format("s3://" + S3_TARGET_BUCKET + "/" + S3_TARGET_PREFIX))
+    print("chunk info")
+    df.info()
 
-  s3_written_objs = wr.s3.to_parquet(
-    df = df,
-    path = "s3://" + S3_TARGET_BUCKET + "/" + S3_TARGET_PREFIX + current_path,
-    partition_cols=  ["line_item_usage_account_id"] if PARTITION_BY_ACCOUNT else [],
-    mode = "overwrite",
-    compression="snappy",
-    index=False,
-    dataset = True
-  )
+    print('Merging account tags to df')
+    df.set_index('identity_line_item_id')
+    df = df.merge(account_tags, left_on='line_item_usage_account_id', right_on='account_tag__account_id', how="left")
+  
+    print('Dropping redundant column on df used for merge: account_tag__account_id')
+    df.drop(columns=['account_tag__account_id'], inplace = True)
 
-  # Keep the last schema for the Glue schema update below - don't want to carry the DataFrame var with us everywhere
-  LAST_SCHEMA_FROM_CUR = wr._data_types.athena_types_from_pandas(df, index=False)
-  print("Wrote: {}".format(s3_written_objs))
-  print("Schema captured: {}".format(LAST_SCHEMA_FROM_CUR))
-  print("=============")
-  df = None
-  gc.collect()
+    print("Original Rows: {} Merged Rows: {}".format(df.shape[0], df.shape[0]))
+    with pd.option_context('display.max_rows', None, 'display.max_columns', None): print(df.columns)
+  
+    # write the merged dFrames into the enriched bucket
+    print ("Writing df to " + "s3://" + S3_TARGET_BUCKET + "/" + S3_TARGET_PREFIX + current_path)
+    s3_written_objs = wr.s3.to_parquet(
+      df = df,
+      path = "s3://" + S3_TARGET_BUCKET + "/" + S3_TARGET_PREFIX + current_path,
+      partition_cols = ["line_item_usage_account_id"] if PARTITION_BY_ACCOUNT else [],
+      mode = "append",
+      compression = "snappy",
+      index = False,
+      dataset = True
+    )
+    print("Wrote: {}".format(s3_written_objs))
 
-
+print("Looking up glue databases")
 databases = wr.catalog.databases()
 
+print("Checking if database has already been created")
 if DATABASE_NAME not in databases.values:
-    wr.catalog.create_database(DATABASE_NAME)
-    print(wr.catalog.databases())
+  print("Database does not exist, creating")
+  wr.catalog.create_database(DATABASE_NAME)
 else:
-    print("Database {} already exists".format(DATABASE_NAME))
+  print("Database {} already exists".format(DATABASE_NAME))
 
+print("Checking if table needs to be created")
 if CREATE_TABLE:
   if OVERWRITE_EXISTING_TABLE:
     print("Deleting {}.{} if it exists".format(DATABASE_NAME, TABLE_NAME))
@@ -215,21 +214,22 @@ if CREATE_TABLE:
       database=DATABASE_NAME,
       table=TABLE_NAME
     )
+  
+  wr._data_types.athena_types_from_pandas(df, index=False)
 
   print ("Creating Table {}.{} with path {}".format(DATABASE_NAME, TABLE_NAME, "s3://" + S3_TARGET_BUCKET + "/" + S3_TARGET_PREFIX)) 
   wr.catalog.create_parquet_table(
     path = "s3://" + S3_TARGET_BUCKET + "/" + S3_TARGET_PREFIX,
     database = DATABASE_NAME,
     table = TABLE_NAME,
-    columns_types = LAST_SCHEMA_FROM_CUR,
-    partitions_types = {'year': 'string','month':'string'} 
+    columns_types = wr._data_types.athena_types_from_pandas(df, index=False),
+    partitions_types = {'year': 'string', 'month':'string'} 
   )
-
 
   print ("Updated Table Partitions {}.{}".format(DATABASE_NAME, TABLE_NAME))
   wr.athena.repair_table(
-    database=DATABASE_NAME,
-    table=TABLE_NAME
+    database = DATABASE_NAME,
+    table = TABLE_NAME
   ) 
 
-print ("Finished")
+print ("CUR enrichment completed")
